c56e7c13c6c8b9183fb0b48a3c190399e568c9d5
Adam Patterson <adpatter@umich.edu>
2021-04-30 18:59:12 -0400

    Start chat.

--- public/script.js
+++ public/script.js
@@ -1,44 +1,39 @@
 window.addEventListener("load", function () {
 
-    let idElement = document.getElementById("id");
-    let resourceElement = document.getElementById("resource");
-    let responseElement = document.getElementById("response");
-    let getButton = document.getElementById("get-button");
-    let putButton = document.getElementById("put-button");
-    let postButton = document.getElementById("post-button");
+    let messageWindow = document.getElementById('message-window');
+    let button = document.getElementsByTagName('button')[0];
 
-    getButton.addEventListener("click", async function () {
+    button.addEventListener('click', async function () {
 
-        let response = await fetch("http://localhost:3000/products/" + idElement.value, {
-            method: 'GET'
-        });
+        try {
 
-        let body = await response.text();
+            let message = messageWindow.value;
 
-        responseElement.innerHTML = body;
+            messageWindow.value = "";
+    
+            let body = JSON.stringify({ data: message });
+    
+            let response = await fetch('http://localhost:3000/api', {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: body
+            });
+
+            if (response.ok === false) {
+
+                console.log("response.ok === false");
+                
+                throw(new Error(response.status));
+            }
+        }
+        catch(e) {
+            console.error(e);
+        }
+
     });
 
-    putButton.addEventListener("click", async function () {
-
-        let response = await fetch("http://localhost:3000/products/" + idElement.value, {
-            method: 'PUT',
-            cache: 'no-cache',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: resourceElement.value
-        });
-    });
-
-    postButton.addEventListener("click", async function () {
-
-        let response = await fetch("http://localhost:3000/products", {
-            method: 'POST',
-            cache: 'no-cache',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: resourceElement.value
-        });
-    });
 });
