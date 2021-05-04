d889e433c8841e8510f97634a564d3b9a0ffa27a
Adam Patterson <adpatter@umich.edu>
2021-05-04 17:50:36 -0400

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.use(express.static('./public'));
 
